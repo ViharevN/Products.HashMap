77a07b8b8f8556f5e1a55d79dc429236e2a2143a
ViharevN <swix_off@mail.ru>
2022-11-27 23:53:18 +0300

    задание 1 часть 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Set<Products> productsList = new HashSet<>();
+
+        Products bananas = new Products("Бананы", 99);
+        Products orange = new Products("Апельсины", 130);
+        Products mandarin = new Products("Мандарины", 120);
+        Products apple = new Products("Яблоки", 100);
+        Products pineapple = new Products("Ананас", 200);
+        Products tomato = new Products("Томаты", 80);
+
+        ServiceProducts products = new ServiceProducts();
+        products.add(bananas);
+//        products.add(bananas);
+        products.add(orange);
+        products.add(mandarin);
+        products.add(apple);
+        products.add(pineapple);
+        products.add(tomato);
+
+        tomato.setChecked();
+
+        products.delete(orange);
+
+        System.out.println(products.toString());
+
+//        Recipe fruitSalad = new Recipe("Фруктовый салат", fruits);
+//        fruits.add(orange);
+//        fruits.add(mandarin);
+//        fruits.add(apple);
+
+//        System.out.println();
+//        System.out.println(fruitSalad);
+//        System.out.println(fruitSalad.getSum());
+
+        Products rice = new Products("рис", 60);
+        Products meat = new Products("мясо", 400);
+        Products oil = new Products("масло", 160);
+
+        Recipe riceAndMeat = new Recipe("Рис с мясом");
+
+        riceAndMeat.addProduct(rice, 2);
+        riceAndMeat.addProduct(meat, 4);
+        riceAndMeat.addProduct(oil, 1);
+
+        System.out.println(riceAndMeat.toString());
+        System.out.println(riceAndMeat.getSum());
     }
 }
---
+++ src/ServiceRecipe.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ServiceRecipe {
+    private final Set<Recipe> productsForRecipe = new HashSet<>();
+
+
+    public boolean addRecipeProducts(Recipe recipe) {
+        if (!productsForRecipe.contains(recipe)) {
+            throw new RuntimeException("Рецепт уже есть");
+        }
+        productsForRecipe.add(recipe);
+        return true;
+    }
+
+}
